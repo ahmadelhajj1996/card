2b27f21fb234fbf612d14ff16975bed044685a2d
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2026-06-23 23:12:42 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import AppRoutes from "./config/router";
+ 
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          width: "794px",
+          minHeight: "1123px",
+          margin: "0 auto",
+          boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+          boxSizing: "border-box",
+        }}
+        className=" p-3"
+      >
+        <AppRoutes />
+      </div>
+
+  
+    </>
+  );
+}
+
+export default App;
